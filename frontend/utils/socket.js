010a84e8f7dd6ef311a3569644c91b6bacbff5b7
Yarva Pavani <139239964+yarvapavani123@users.noreply.github.com>
2026-05-19 10:48:36 +0530

    Update socket.js

--- frontend/utils/socket.js
+++ frontend/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io(process.env.NEXT_PUBLIC_API_URL, {
   reconnection: true,
   reconnectionAttempts: 5,
 });
