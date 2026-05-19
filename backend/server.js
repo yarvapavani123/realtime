0ad92570a91499a3190c9c411eb35ba40f82eee9
Jahnavi Sigatapu <sigatapujahnavi77@gmail.com>
2026-05-19 14:47:16 +0530

    fix: add realtime-sage to CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://realtime-chi-peach.vercel.app"
+  "https://realtime-chi-peach.vercel.app",
+  "https://realtime-sage.vercel.app"
 ];
 
 app.use(cors({
